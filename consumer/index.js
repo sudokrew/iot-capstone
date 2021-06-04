dd2fc3a17da9330e0a8692b241575dc24693b49c
Nicholas Gambino <nicholasgambino@Nicholass-MacBook-Pro-2.local>
2021-06-04 12:53:04 -1000

    Remove comments

--- consumer/index.js
+++ consumer/index.js
@@ -5,11 +5,6 @@ const { hostname } = require("os");
 const { INFLUX_URL, INFLUX_TOKEN, INFLUX_ORG, INFLUX_BUCKET, RABBIT_MQ } =
   process.env;
 
-// const url = process.env["INFLUX_URL"] || "http://127.0.0.1:8086";
-// const token = process.env["INFLUX_TOKEN"] || "my-token";
-// const org = process.env["INFLUX_ORG"] || "iot_capstone_org";
-// const bucket = "iot_capstone_db";
-
 const url = INFLUX_URL;
 const token = INFLUX_TOKEN;
 const org = INFLUX_ORG;
